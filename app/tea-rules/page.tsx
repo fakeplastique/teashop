2a1c0e841c4afedaf906cc4d80ea27db90c457b9
fakeplastique <ihor.bondar.work@gmail.com>
2025-11-30 00:39:58 +0200

    making tearules page editable for admins

--- app/tea-rules/page.tsx
+++ app/tea-rules/page.tsx
@@ -1,15 +1,13 @@
 import { getDataSource } from "@/lib/db";
-import { seedDatabase } from "@/lib/seed";
 import { StaticPage } from "@/entities/StaticPage";
+import EditableContent from "@/components/admin/EditableContent";
 import styles from "./page.module.scss";
 
-// This page will be statically generated at build time
-export const dynamic = "force-static";
-export const revalidate = 3600; // Revalidate every hour
+export const dynamic = "force-dynamic";
+export const revalidate = 3600;
 
 export default async function TeaRulesPage() {
   const dataSource = await getDataSource();
-  await seedDatabase();
 
   const staticPageRepository = dataSource.getRepository(StaticPage);
   const page = await staticPageRepository.findOne({
@@ -30,10 +28,7 @@ export default async function TeaRulesPage() {
   return (
     <div className={styles.container}>
       <article className={styles.article}>
-        <div
-          className={styles.content}
-          dangerouslySetInnerHTML={{ __html: page.content }}
-        />
+        <EditableContent initialContent={page.content} pageSlug="tea-rules" />
 
         <div className={styles.footer}>
           <p className={styles.note}>
